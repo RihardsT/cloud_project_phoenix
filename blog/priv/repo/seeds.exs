98cfb0dbff1e2949bf1dfcca757eaebfe1d1852a
Rihards Teteris <richitislv@gmail.com>
2019-06-16 11:44:42 +0300

    Use alias in seeds.exs

--- blog/priv/repo/seeds.exs
+++ blog/priv/repo/seeds.exs
@@ -10,8 +10,9 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 import Ecto.Query
+alias Blog.{Repo, Articles.Article}
 
-unless(Blog.Repo.one(from article in Blog.Articles.Article, select: count(article.id)) != 0) do
-  Blog.Repo.insert( Blog.Articles.Article.changeset( %Blog.Articles.Article{}, %{title: "First article", content: "Content of first article", published: Date.utc_today()} ))
-  Blog.Repo.insert( Blog.Articles.Article.changeset( %Blog.Articles.Article{} ,%{title: "Second article", content: "Content of second article", published: Date.utc_today()} ))
+unless(Repo.one(from article in Article, select: count(article.id)) != 0) do
+  Repo.insert( Article.changeset( %Article{}, %{title: "First article", content: "Content of first article", published: Date.utc_today()} ))
+  Repo.insert( Article.changeset( %Article{} ,%{title: "Second article", content: "Content of second article", published: Date.utc_today()} ))
 end
